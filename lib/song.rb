274eb39e8624abe1beda39b2b9df592407c0b019
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-07-11 14:01:46 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+class Song
+  @@count=0
+  @@artists=[]
+  @genres=[]
+  attr_accessor :name, :artist, :genre
+  
+  def initialize(name, artist, genre)
+    @name=name
+    @artist= artist
+    @genre=genre
+
+    @@count+=1
+end
+
+  
+  
+end
